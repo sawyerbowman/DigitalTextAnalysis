4fef56cf7f13d66604769fbc21cc4b3881784331
sawyerbowman <sawyerbowman@gmail.com>
2015-04-13 16:41:21 -0400

    Initial Commit

---
+++ DigitalTextAnalysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DigitalTextAnalysis
+//
+//  Created by Sawyer Bowman on 4/13/15.
+//  Copyright (c) 2015 Sawyer Bowman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
